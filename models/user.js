9df0e6825fec9d3b8fdc24c2d138a9262b5ebf10
dayeondayeon <dayeon2438@gmail.com>
2021-08-13 23:18:56 +0900

    Feat : user db 생성

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize');
+
+module.exports = class User extends Sequelize.Model {
+    static init(sequelize) {
+        return super.init({
+            nickname : {
+                type : Sequelize.STRING(10),
+                allowNull : false,
+                unique : true,
+            }, 
+            email : {
+                type : Sequelize.STRING(30),
+                allowNull : false,
+                unique : true,
+            },
+            pw : {
+                type : Sequelize.STRING(20),
+                allowNull : false,
+            },
+        }, {
+            sequelize,
+            timestamps : true,
+            underscored : false, 
+            modelName : 'User', 
+            tableName : 'users', 
+            paranoid : false,
+            charset : 'utf8',
+            collate : 'utf8_general_ci',
+        });
+    }
+    static associate(db) {}
+};
