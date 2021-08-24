cd0291cc8e76009334e01e5ddea3335d745f25ed
dayeondayeon <dayeon2438@gmail.com>
2021-08-24 16:28:04 +0900

    Feat : 회원가입시 비밀번호 암호화 기능 구현

--- routes/signup.js
+++ routes/signup.js
@@ -4,13 +4,14 @@ const connection = require('../modules/mysql');
 const statusCode = require('../modules/status');
 const messageCode = require('../modules/message');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 //회원가입
 router.post('/join', async (req, res) => {
     const { nickname, email, pw } = req.body;
-
+    const hash = await bcrypt.hash(pw, 12);
     // 삽입 수행
-    const sql = `INSERT INTO User (nickname, email, pw) VALUES ('${nickname}', '${email}', '${pw}')`;    
+    const sql = `INSERT INTO User (nickname, email, pw) VALUES ('${nickname}', '${email}', '${hash}')`;    
     
     connection.query(sql, function (err, result) {        
         var resultCode = statusCode.NOT_FOUND;
