ab4f040ad5b46e2d4fb3350ce2d1e75e7a027663
dayeondayeon <dayeon2438@gmail.com>
2021-08-29 00:44:54 +0900

    Fix : 로그인 암호화 오류 수정

--- routes/signin.js
+++ routes/signin.js
@@ -6,24 +6,24 @@ const messageCode = require('../modules/message');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
-router.post('/signin', (req, res) => {
+router.post('/signin', async (req, res) => {
     const {
         email,
         pw
     } = req.body;
     const sql = `SELECT * FROM User WHERE email="${email}"`;
-    const hash = await bcrypt.hash(pw, 12);
     connection.query(sql, function (err, rows, fields) {
         if (err) {
             console.log(err);
             return res.status(statusCode.NOT_FOUND).send(messageCode.REQUEST_FAIL);
         }
         else {
+            const match = bcrypt.compare(pw, rows[0].pw);
             if (rows.length === 0) {
                 return res.status(statusCode.MATCH_ERR).send(messageCode.INVALID_USER);
             }
             
-            else if (hash !== rows[0].pw) {
+            else if (!match) {
                 return res.status(statusCode.MATCH_ERR).send(messageCode.INVALID_PW);
             } 
             else {
