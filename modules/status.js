ac6c3b51d688c553350a3d6b07a5ebe77ed9f861
Happiva <kiwhhb1016@gmail.com>
2021-08-31 03:46:00 +0900

    Modify : 서버 응답을 JSON 형식으로 보내게 변경

--- modules/status.js
+++ modules/status.js
@@ -2,5 +2,7 @@ module.exports = {
     SUCCESS : 200,
     MATCH_ERR : 204,
     BAD_REQUEST : 400,
-    NOT_FOUND : 404
+    UNAUTHORIZED: 401,
+    NOT_FOUND : 404,    
+    TOKEN_EXPIRED: 419,
 };
